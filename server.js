22a5b96fb850c03955266b9750d7aaec6be11a97
JLA <arancibiajav@gmail.com>
2014-05-05 14:51:04 -0300

    Now working. package.json its ready now.

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
-var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , reload = require('reload')
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var reload = require('reload');
+var morgan  = require('morgan');
+var app = express();
+var publicDir = path.join(__dirname, 'public');
+var bodyParser = require("body-parser");
 
-var app = express()
-
-var publicDir = path.join(__dirname, 'public')
-
-app.configure(function() {
-  app.set('port', process.env.PORT || 3000)
-  app.use(express.logger('dev'))
-  app.use(express.bodyParser()) //parses json, multi-part (file), url-encoded
-  app.use(app.router) //need to be explicit, (automatically adds it if you forget)
-  app.use(express.static(clientDir)) //should cache static assets
-})
+app.set('port', process.env.PORT || 3000)
+app.use(morgan());
+app.use(bodyParser());
 
 app.get('/', function(req, res) {
   res.sendfile(path.join(publicDir, 'index.html'))
 })
 
 var server = http.createServer(app)
-
-//reload code here
-reload(server, app)
-
+reload(server, app);
 server.listen(app.get('port'), function(){
   console.log("Web server listening on port " + app.get('port'));
 });
